fd7fa3b4cd88ef112503534a1bde704a623fa2bc
Yigit Ozgumus <yigitozgumus1@gmail.com>
2020-11-21 12:41:33 +0300

    change dotfile management to code, add installation scripts

--- install_system.sh
+++ install_system.sh
@@ -1,13 +1,19 @@
+#!/bin/zsh
+
 # This will be main system installation script
+./scripts/main.sh
 
 # install the package manager depending on the OS
+./scripts/packageManager.sh
 
 # install the required barebone programs from the package manager
+./scripts/programs.sh
 
 # install the terminal requirements
+./scripts/terminal.sh
 
 # install Python related setup
+./scripts/python.sh
 
-# install other language and program requirements
-
-# install additional programs
+# # install other language and program requirements
+./scripts/js.sh
