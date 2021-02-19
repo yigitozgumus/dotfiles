c30ac091ec608ab53a11a7d51f9de9670d774f67
Yigit Ozgumus <yigitozgumus1@gmail.com>
2021-02-19 17:15:26 +0300

    lazyigt fix

--- fish/lazygit_theme.fish
+++ fish/lazygit_theme.fish
@@ -3,11 +3,12 @@ function lazygit_theme --argument theme
         echo "Lazygit config file doesn't exist"
         return
     end
-    set -l config_path (realpath ~/Library/ApplicationSupport/jesseduffield/lazygit/config.yml)
+    set -l config_path (~/Library/ApplicationSupport/jesseduffield/lazygit/config.yml)
+    echo $config_path
     switch theme
-        case light
-            sed -i '' 's/--dark/--light/g' $config_path
-        case dark
-            sed -i '' 's/--light/--dark/g' $config_path
-        end
+      case light
+        sed   "s/--dark/--light/g" $config_path
+      case dark
+        sed   "s/--light/--dark/g" $config_path
+    end
 end
