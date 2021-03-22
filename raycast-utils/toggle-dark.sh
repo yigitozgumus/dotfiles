a63acc1a7656b9870f022685d7c8b754c197c91b
Yigit Ozgumus <yigitozgumus1@gmail.com>
2021-03-22 23:22:18 +0300

    add raycast util scripts

---
+++ raycast-utils/toggle-dark.sh
@@ -0,0 +1,18 @@
+#!/usr/local/bin/fish
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Toggle dark
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🌒
+# @raycast.packageName developer utils
+
+# @Documentation:
+# @raycast.description turns all system configuration to dark mode
+# @raycast.author Yigit Ozgumus
+# @raycast.authorURL www.twitter.com/yigitozgumus
+change_background "dark"
+echo "Dark mode enabled"
+
---
+++ raycast-utils/toggle-light.sh
@@ -0,0 +1,20 @@
+#!/usr/local/bin/fish
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Toggle Light
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🌞
+# @raycast.packageName developer utils
+
+# @Documentation:
+# @raycast.description turns system preferences to light mode
+# @raycast.author Yigit Ozgumus
+# @raycast.authorURL www.twitter.com/yigitozgumus
+
+change_background "light"
+
+echo "$1 Mode Enabled."
+
