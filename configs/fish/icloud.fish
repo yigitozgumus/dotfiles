b98a3096a2335eaf12073c227d81590ffd26a5ea
Yigit Ozgumus <yigitozgumus1@gmail.com>
2021-06-10 16:09:47 +0300

    add icloud alias

---
+++ configs/fish/icloud.fish
@@ -0,0 +1,3 @@
+function icloud --wraps='cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/' --description 'alias icloud=cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/'
+  cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/ $argv;
+end
