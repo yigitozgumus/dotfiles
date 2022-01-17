b5b168686398546c3f28ac28d263e3f28d845a82
Yigit Ozgumus <yigitozgumus1@gmail.com>
2022-01-17 22:44:09 +0300

    update dotfile config

--- auto_dark_mode.py
+++ auto_dark_mode.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python3
 
-#!/usr/bin/env python3
-
 import asyncio
 import iterm2
 
-async def changeTheme(connection,parts):
-    theme_dark = "Tango Dark"
-    theme_light = "Tango Light"
+
+async def changeTheme(connection, parts):
+    theme_dark = "Bright Lights"
+    theme_light = "primary"
     print(parts)
-    
+
     if "dark" in parts:
         preset = await iterm2.ColorPreset.async_get(connection, theme_dark)
     else:
         preset = await iterm2.ColorPreset.async_get(connection, theme_light)
 
     # Update the list of all profiles and iterate over them.
-    profiles=await iterm2.PartialProfile.async_query(connection)
+    profiles = await iterm2.PartialProfile.async_query(connection)
     for partial in profiles:
         # Fetch the full profile and then set the color preset in it.
         profile = await partial.async_get_full_profile()
         await profile.async_set_color_preset(preset)
 
+
 async def main(connection):
 
     app = await iterm2.async_get_app(connection)
-    window = app.current_window
     initial_theme = await app.async_get_theme()
-    await changeTheme(connection,initial_theme)
+    await changeTheme(connection, initial_theme)
 
     async with iterm2.VariableMonitor(connection, iterm2.VariableScopes.APP, "effectiveTheme", None) as mon:
         while True:
@@ -35,8 +34,8 @@ async def main(connection):
             theme = await mon.async_get()
             # Themes have space-delimited attributes, one of which will be light or dark.
             parts = theme.split(" ")
-    
-            await changeTheme(connection,parts)
-            
+
+            await changeTheme(connection, parts)
+
 
 iterm2.run_forever(main)
