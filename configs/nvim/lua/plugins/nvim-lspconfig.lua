b35fa8e0fda60ef3de845c2f15623614e5bf0f60
Yiğit Özgümüş <yigitozgumus1@gmail.com>
2022-05-27 19:05:32 +0300

    remove kotlin server

--- configs/nvim/lua/plugins/nvim-lspconfig.lua
+++ configs/nvim/lua/plugins/nvim-lspconfig.lua
@@ -124,7 +124,7 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches.
 -- Add your language server below:
-local servers = { 'bashls', 'pyright', 'clangd', 'html', 'cssls', 'tsserver', 'kotlin-language-server' }
+local servers = { 'bashls', 'pyright', 'clangd', 'html', 'cssls', 'tsserver', 'gopls' }
 
 -- Call setup
 for _, lsp in ipairs(servers) do
