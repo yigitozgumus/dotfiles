46b0f88b93f9de5810c16716bd419f4929050443
Yigit Ozgumus <yigitozgumus1@gmail.com>
2022-05-28 17:06:20 +0300

    add options

--- configs/nvim/lua/user/options.lua
+++ configs/nvim/lua/user/options.lua
@@ -14,16 +14,12 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.wildmode = 'longest:full,full'
 vim.o.wrap = false
-vim.o.list = true
-vim.o.listchars = 'tab:▸ ,trail:·'
 vim.o.mouse = 'a'
 vim.o.splitright = true
 vim.o.splitbelow = true
 vim.o.scrolloff = 8
 vim.o.sidescrolloff = 8
 vim.o.clipboard = 'unnamedplus' -- Use Linux system clipboard
-vim.o.list = true
-vim.o.listchars = 'tab:▸ ,trail:·'
 vim.o.confirm = true
 vim.o.backup = true
 vim.o.backupdir = vim.fn.stdpath 'data' .. '/backup//'
@@ -31,13 +27,9 @@ vim.o.updatetime = 250          -- Decrease CursorHold delay
 vim.o.redrawtime = 10000        -- Allow more time for loading syntax on large files
 vim.o.showmode = false
 vim.o.fillchars = 'eob: '
-
-
 -----------------------------------------------------------
 -- Startup
 -----------------------------------------------------------
--- Disable nvim intro
-vim.opt.shortmess:append "sI"
 
 -- Disable builtins plugins
 local disabled_built_ins = {
