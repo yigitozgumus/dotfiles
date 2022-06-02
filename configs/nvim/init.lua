88b403468283828a8760ddc013f6aaa9bd136b64
Yiğit Özgümüş <yigitozgumus1@gmail.com>
2022-06-02 14:11:10 +0300

    update color settings

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -7,22 +7,5 @@ require 'user.misc'
 require 'user.colors'
 require 'user.autocmds'
 
--- Colorscheme
--- Lua:
--- For dark theme (neovim's default)
-vim.o.background = "dark"
--- For light theme
--- jjvim.o.background = "light"
--- Enable transparent background
-vim.g.vscode_transparent = 1
-
--- vim.g.tokyonight_transparent = 1
-
--- Enable italic comment
-vim.g.vscode_italic_comment = 1
--- Disable nvim-tree background color
-vim.g.vscode_disable_nvimtree_bg = true
--- Potential colorschemes: vscode onedark tokyonight duskfox
-vim.cmd([[colorscheme tokyonight]])
 
 -- vim: nospell
