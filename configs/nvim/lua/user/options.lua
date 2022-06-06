2a88a8eb76cdc4762df244da41f791fb13dabbd6
Yiğit Özgümüş <yigitozgumus1@gmail.com>
2022-06-06 07:42:07 +0300

    Remove core package and update options file

--- configs/nvim/lua/user/options.lua
+++ configs/nvim/lua/user/options.lua
@@ -1,37 +1,75 @@
 
+-----------------------------------------------------------
+-- General Neovim settings and configuration
+-----------------------------------------------------------
 
+-----------------------------------------------------------
+-- General
+-----------------------------------------------------------
+vim.o.mouse = 'a'
+vim.o.clipboard = 'unnamedplus' -- Use Linux system clipboard
+vim.o.swapfile = false
+vim.o.completeopt = 'menuone,noinsert,noselect'
+
+
+-----------------------------------------------------------
+-- Neovim UI
+-----------------------------------------------------------
+vim.o.number = true
+vim.o.showmatch = true
+vim.o.foldmethod = 'marker'   -- Enable folding (default 'foldmarker')
+vim.o.colorcolumn = '80'      -- Line lenght marker at 80 columns
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.linebreak = true
+vim.o.relativenumber = true
+vim.o.termguicolors = true    -- Enable 24-bit RGB colors
+vim.o.laststatus=2            -- Set global statusline
+
+-----------------------------------------------------------
+-- Tabs, indent
+-----------------------------------------------------------
 vim.o.expandtab = true
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
+vim.o.smartindent = true
+
+-----------------------------------------------------------
+-- Memory, CPU
+-----------------------------------------------------------
+
+vim.o.hidden = true           -- Enable background buffers
+vim.o.history = 10000           -- Remember N lines in history
+vim.o.lazyredraw = true       -- Faster scrolling
+vim.o.synmaxcol = 240         -- Max column for syntax highlight
+vim.o.updatetime = 700        -- ms to wait for trigger an event
+-- vim.o.updatetime = 250          -- Decrease CursorHold delay
+-- vim.o.redrawtime = 10000        -- Allow more time for loading syntax on large files
+
+-----------------------------------------------------------
+-- Misc
+-----------------------------------------------------------
 vim.o.signcolumn = 'yes:2'
-vim.o.relativenumber = true
-vim.o.number = true
-vim.o.termguicolors = true
 vim.o.undofile = true
 vim.o.spell = true
 vim.o.title = true
-vim.o.ignorecase = true
-vim.o.smartcase = true
 vim.o.wildmode = 'longest:full,full'
 vim.o.wrap = false
-vim.o.mouse = 'a'
-vim.o.splitright = true
-vim.o.splitbelow = true
 vim.o.scrolloff = 8
 vim.o.sidescrolloff = 8
-vim.o.clipboard = 'unnamedplus' -- Use Linux system clipboard
 vim.o.confirm = true
 vim.o.backup = true
 vim.o.backupdir = vim.fn.stdpath 'data' .. '/backup//'
-vim.o.updatetime = 250          -- Decrease CursorHold delay
-vim.o.redrawtime = 10000        -- Allow more time for loading syntax on large files
 vim.o.showmode = false
 vim.o.fillchars = 'eob: '
-vim.o.swapfile = false
 vim.cmd [[
 setlocal path=.,**
 set iskeyword+=-
 ]]
+
+
 -----------------------------------------------------------
 -- Startup
 -----------------------------------------------------------
