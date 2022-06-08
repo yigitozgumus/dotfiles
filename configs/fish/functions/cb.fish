c263464938f6ed252c13d7f3bd648b100aefa6ec
Yigit Ozgumus <yigitozgumus1@gmail.com>
2022-06-08 09:47:25 +0300

    update change background for different tmux windows

--- configs/fish/functions/cb.fish
+++ configs/fish/functions/cb.fish
@@ -24,7 +24,7 @@ function cb --argument mode_setting
   # iterate over all vim sessions and change the background ourself.
   for tms in  (/opt/homebrew/bin/tmux list-sessions -F '#{session_name}')
     for wix in (/opt/homebrew/bin/tmux list-windows -t $tms -F "$tms:#{window_index}")
-      for pix in (/opt/homebrew/bin/tmux list-panes -F 'main:#{window_index}.#{pane_index}' -t $wix)
+      for pix in (/opt/homebrew/bin/tmux list-panes -F "$tms:#{window_index}.#{pane_index}" -t $wix)
         set -l is_vim "ps -o state= -o comm= -t '#{pane_tty}'  | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|n?vim?x?)(diff)?\$'"
         /opt/homebrew/bin/tmux if-shell -t "$pix" "$is_vim" "send-keys -t $pix escape ENTER"
         /opt/homebrew/bin/tmux if-shell -t "$pix" "$is_vim" "send-keys -t $pix ':call ChangeBackground()' ENTER"
