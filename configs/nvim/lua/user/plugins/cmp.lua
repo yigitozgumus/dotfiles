42e8bf7e9a16251e57e58b67f5439bed349c7ad8
Yigit Ozgumus <yigitozgumus1@gmail.com>
2022-07-29 14:59:21 +0300

    fix cmp for tab completion

--- configs/nvim/lua/user/plugins/cmp.lua
+++ configs/nvim/lua/user/plugins/cmp.lua
@@ -39,44 +39,26 @@ local has_words_before = function()
         behavior = cmp.ConfirmBehavior.Replace,
         select = false,
       },
-     -- ['<Tab>'] = cmp.mapping(function(fallback)
-     --   if luasnip.jumpable(1) then
-     --     luasnip.jump(1)
-     --   elseif cmp.visible() then
-     --     cmp.select_next_item()
-     --   elseif has_words_before() then
-     --     cmp.complete()
-     --   else
-     --     fallback()
-     --   end
-
-        -- if cmp.visible() then
-        --   cmp.select_next_item()
-        -- elseif luasnip.expand_or_jumpable() then
-        --   luasnip.expand_or_jump()
-        -- elseif has_words_before() then
-        --   cmp.complete()
-        -- else
-        --   fallback()
-        -- end
---      end, { 'i', 's' }),
---       ['<S-Tab>'] = cmp.mapping(function(fallback)
---         if luasnip.jumpable(-1) then
---           luasnip.jump(-1)
---         elseif cmp.visible() then
---           cmp.select_prev_item()
---         else
---           fallback()
---         end
-
-        -- if cmp.visible() then
-        --   cmp.select_prev_item()
-        -- elseif luasnip.jumpable(-1) then
-        --   luasnip.jump(-1)
-        -- else
-        --   fallback()
-        -- end
---       end, { 'i', 's' }),
+      ['<Tab>'] = cmp.mapping(function(fallback)
+        if luasnip.jumpable(1) then
+          luasnip.jump(1)
+        elseif cmp.visible() then
+          cmp.select_next_item()
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end, { 'i', 's' }),
+       ['<S-Tab>'] = cmp.mapping(function(fallback)
+         if luasnip.jumpable(-1) then
+           luasnip.jump(-1)
+         elseif cmp.visible() then
+           cmp.select_prev_item()
+         else
+           fallback()
+         end
+       end, { 'i', 's' }),
     },
     sources = {
       { name = 'nvim_lsp' },
