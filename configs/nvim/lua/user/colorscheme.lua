0eeba46100e85766a34f30717818ad88334fac57
Yiğit Özgümüş <yigitozgumus1@gmail.com>
2022-07-30 12:01:11 +0300

    update kitty conf

--- configs/nvim/lua/user/colorscheme.lua
+++ configs/nvim/lua/user/colorscheme.lua
@@ -3,14 +3,13 @@
 -- Lua:
 vim.o.background = "dark"
 -- For light theme
--- jjvim.o.background = "light"
 -- Enable transparent background
 vim.g.vscode_transparent = 1
 vim.g.tokyonight_transparent = 1
 -- Enable italic comment
 vim.g.vscode_italic_comment = 1
 -- Disable nvim-tree background color
-vim.g.vscode_disable_nvimtree_bg = true
+-- vim.g.vscode_disable_nvimtree_bg = true
 -- Potential colorschemes: vscode onedark tokyonight duskfox
 vim.cmd([[colorscheme duskfox]])
 
