8cf5188c0bee1697273a71bba3b0bc04a264bcd1
Yiğit Özgümüş <yigitozgumus1@gmail.com>
2022-08-16 21:25:12 +0300

    update nvim config

---
+++ configs/nvim/after/plugin/go.rc.lua
@@ -0,0 +1,10 @@
+local status, go = pcall(require, "go")
+if (not status) then return end
+go.setup()
+
+local statusFormat, goformat = pcall(require, "go.format")
+if (not statusFormat) then return end
+goformat.goimport()
+
+-- Run gofmt + goimport on save
+vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').goimport() ]], false)
--- configs/nvim/lua/plugins.lua
+++ configs/nvim/lua/plugins.lua
@@ -7,50 +7,53 @@ end
 vim.cmd [[packadd packer.nvim]]
 
 packer.startup(function(use)
-    use {'wbthomason/packer.nvim'} -- Let packer manage itself
-    use {
-        'svrana/neosolarized.nvim',
-        requires = { 'tjdevries/colorbuddy.nvim' }
-      }
-      use 'nvim-lualine/lualine.nvim' -- Statusline
-      use 'nvim-lua/plenary.nvim' -- Common utilities
-      use 'onsails/lspkind-nvim' -- vscode-like pictograms
-      use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
-      use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
-      use 'hrsh7th/nvim-cmp' -- Completion
-      use 'neovim/nvim-lspconfig' -- LSP
-      use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
-      use 'MunifTanjim/prettier.nvim' -- Prettier plugin for Neovim's built-in LSP client
-      use 'williamboman/mason.nvim'
-      use 'folke/tokyonight.nvim'
-      use 'williamboman/mason-lspconfig.nvim'
-      use 'glepnir/lspsaga.nvim' -- LSP UIs
-      use 'L3MON4D3/LuaSnip'
-      use { 'EdenEast/nightfox.nvim' }
-      use {
-        'kyazdani42/nvim-tree.lua',
-        requires = {
-          'kyazdani42/nvim-web-devicons', -- optional, for file icons
-        },
-        tag = 'nightly' -- optional, updated every week. (see issue #1193)
-      }
-      use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
-      }
-      use 'kyazdani42/nvim-web-devicons' -- File icons
-      use 'nvim-telescope/telescope.nvim'
-      use 'nvim-telescope/telescope-file-browser.nvim'
-      use 'windwp/nvim-autopairs'
-      use 'windwp/nvim-ts-autotag'
-      use 'norcalli/nvim-colorizer.lua'
-      use 'folke/zen-mode.nvim'
-      use({
-        "iamcco/markdown-preview.nvim",
-        run = function() vim.fn["mkdp#util#install"]() end,
-      })
-      use 'akinsho/nvim-bufferline.lua'
-    
-      use 'lewis6991/gitsigns.nvim'
-      use 'dinhhuy258/git.nvim' -- For git blame & browse
+  use { 'wbthomason/packer.nvim' } -- Let packer manage itself
+  use {
+    'svrana/neosolarized.nvim',
+    requires = { 'tjdevries/colorbuddy.nvim' }
+  }
+  use 'nvim-lualine/lualine.nvim' -- Statusline
+  use 'nvim-lua/plenary.nvim' -- Common utilities
+  use 'onsails/lspkind-nvim' -- vscode-like pictograms
+  use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
+  use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
+  use 'hrsh7th/nvim-cmp' -- Completion
+  use 'neovim/nvim-lspconfig' -- LSP
+  use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+  use 'MunifTanjim/prettier.nvim' -- Prettier plugin for Neovim's built-in LSP client
+  use 'williamboman/mason.nvim'
+  use 'folke/tokyonight.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
+  use 'glepnir/lspsaga.nvim' -- LSP UIs
+  use 'L3MON4D3/LuaSnip'
+  use { 'EdenEast/nightfox.nvim' }
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icons
+    },
+    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+  use 'kyazdani42/nvim-web-devicons' -- File icons
+  use 'nvim-telescope/telescope.nvim'
+  use 'nvim-telescope/telescope-file-browser.nvim'
+  use 'windwp/nvim-autopairs'
+  use 'windwp/nvim-ts-autotag'
+  use 'norcalli/nvim-colorizer.lua'
+  use 'folke/zen-mode.nvim'
+  use({
+    "iamcco/markdown-preview.nvim",
+    run = function() vim.fn["mkdp#util#install"]() end,
+  })
+  use 'akinsho/nvim-bufferline.lua'
+
+  use 'lewis6991/gitsigns.nvim'
+  use 'dinhhuy258/git.nvim' -- For git blame & browse
+  use 'ray-x/go.nvim'
+  use 'ray-x/guihua.lua' -- recommanded if need floating window support
+
 end)
