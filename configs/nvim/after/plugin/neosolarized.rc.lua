46c195ca400fbcf983ee2174a1fab45646c559b8
Yiğit Özgümüş <yigitozgumus1@gmail.com>
2022-08-16 23:11:16 +0300

    update nvim color scheme

--- configs/nvim/after/plugin/neosolarized.rc.lua
+++ configs/nvim/after/plugin/neosolarized.rc.lua
@@ -13,9 +13,9 @@ local groups = cb.groups
 local styles = cb.styles
 
 Color.new('black', '#000000')
-Group.new('CursorLine', colors.none, colors.base03, styles.NONE, colors.base1)
-Group.new('CursorLineNr', colors.yellow, colors.black, styles.NONE, colors.base1)
-Group.new('Visual', colors.none, colors.base03, styles.reverse)
+-- Group.new('CursorLine', colors.none, colors.base03, styles.NONE, colors.base1)
+-- Group.new('CursorLineNr', colors.yellow, colors.black, styles.NONE, colors.base1)
+-- Group.new('Visual', colors.none, colors.base03, styles.reverse)
 
 local cError = groups.Error.fg
 local cInfo = groups.Information.fg
