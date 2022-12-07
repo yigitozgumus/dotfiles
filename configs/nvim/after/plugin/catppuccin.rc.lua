81684d84715edf6a8cf13ad8fea6d704d8c3904d
Yigit Ozgumus <yigitozgumus1@gmail.com>
2022-12-07 17:45:55 +0300

    fix themeing

--- configs/nvim/after/plugin/catppuccin.rc.lua
+++ configs/nvim/after/plugin/catppuccin.rc.lua
@@ -6,7 +6,7 @@ catppuccin.setup({
     light = "latte",
     dark = "mocha",
   },
-  transparent_background = false,
+  transparent_background = true,
   term_colors = false,
   dim_inactive = {
     enabled = false,
