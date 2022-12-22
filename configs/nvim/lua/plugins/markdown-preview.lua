9a3faf7314180fe08b5648c94b19b282499580a6
Yigit Ozgumus <yigitozgumus1@gmail.com>
2022-12-22 19:02:01 +0300

    update configs

--- configs/nvim/lua/plugins/markdown-preview.lua
+++ configs/nvim/lua/plugins/markdown-preview.lua
@@ -1,9 +1,11 @@
 local pluginActive = true
-if (not pluginActive) then return end
+if not pluginActive then
+	return
+end
 
 -- set to 1, nvim will open the preview window after entering the markdown buffer
 -- default: 0
-vim.g.mkdp_auto_start = 1
+vim.g.mkdp_auto_start = 0
 
 -- set to 1, the nvim will auto close current preview window when change
 -- from markdown buffer to another buffer
@@ -30,14 +32,14 @@ vim.g.mkdp_open_to_the_world = 0
 -- useful when you work in remote vim and preview on local browser
 -- more detail see: https://github.com/iamcco/markdown-preview.nvim/pull/9
 -- default empty
-vim.g.mkdp_open_ip = ''
+vim.g.mkdp_open_ip = ""
 
 -- specify browser to open preview page
 -- for path with space
 -- valid: `/path/with\ space/xxx`
 -- invalid: `/path/with\\ space/xxx`
 -- default: ''
-vim.g.mkdp_browser = ''
+vim.g.mkdp_browser = ""
 
 -- set to 1, echo preview page url in command line when open preview page
 -- default is 0
@@ -46,7 +48,7 @@ vim.g.mkdp_echo_preview_url = 0
 -- a custom vim function name to open preview page
 -- this function will receive url as param
 -- default is empty
-vim.g.mkdp_browserfunc = ''
+vim.g.mkdp_browserfunc = ""
 
 -- options for markdown render
 -- mkit: markdown-it options for render
@@ -65,23 +67,23 @@ vim.g.mkdp_browserfunc = ''
 
 --  use a custom markdown style must be absolute path
 -- like '/Users/username/markdown.css' or expand('~/markdown.css')
-vim.g.mkdp_markdown_css = ''
+vim.g.mkdp_markdown_css = ""
 
 -- use a custom highlight style must absolute path
 -- like '/Users/username/highlight.css' or expand('~/highlight.css')
-vim.g.mkdp_highlight_css = ''
+vim.g.mkdp_highlight_css = ""
 
 -- use a custom port to start server or empty for random
-vim.g.mkdp_port = ''
+vim.g.mkdp_port = ""
 
 -- preview page title
 -- ${name} will be replace with the file name
-vim.g.mkdp_page_title = '「${name}」'
+vim.g.mkdp_page_title = "「${name}」"
 
 -- recognized filetypes
 -- these filetypes will have MarkdownPreview... commands
-vim.g.mkdp_filetypes = { 'markdown', 'md' }
+vim.g.mkdp_filetypes = { "markdown", "md" }
 
 -- set default theme (dark or light)
 -- By default the theme is define according to the preferences of the system
-vim.g.mkdp_theme = 'light'
+vim.g.mkdp_theme = "light"
