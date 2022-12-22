3fcb3bb9e11347120cb9b149f68c4e85c2b06ce2
Yigit Ozgumus <yigitozgumus1@gmail.com>
2022-12-22 18:33:24 +0300

    update plugins

--- configs/nvim/lua/core/keymaps.lua
+++ configs/nvim/lua/core/keymaps.lua
@@ -1,133 +1,135 @@
 local keymap = vim.keymap
 
-vim.g.mapleader = ','
-vim.g.maplocalleader = ','
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
 
-keymap.set('n', '<leader>k', ':nohlsearch<CR>')
-keymap.set('n', '<leader>Q', ':bufdo bdelete<CR>')
+keymap.set("n", "<leader>k", ":nohlsearch<CR>")
+keymap.set("n", "<leader>Q", ":bufdo bdelete<CR>")
 
 -- Allow gf to open non-existent files
-keymap.set('', 'gf', ':edit <cfile><CR>')
+keymap.set("", "gf", ":edit <cfile><CR>")
 
 -- Reselect visual selection after indenting
-keymap.set('v', '<', '<gv')
-keymap.set('v', '>', '>gv')
+keymap.set("v", "<", "<gv")
+keymap.set("v", ">", ">gv")
 
 -- Maintain the cursor position when yanking a visual selection
 -- http://ddrscott.github.io/blog/2016/yank-without-jank/
-keymap.set('v', 'y', 'myy`hay')
-keymap.set('v', 'Y', 'myY`y')
+keymap.set("v", "y", "myy`hay")
+keymap.set("v", "Y", "myY`y")
 
 -- Easy insertion of a trailing ; or , from insert mode
-keymap.set('i', ';;', '<Esc>A;<Esc>')
-keymap.set('i', ',,', '<Esc>A,<Esc>')
+keymap.set("i", ";;", "<Esc>A;<Esc>")
+keymap.set("i", ",,", "<Esc>A,<Esc>")
 
 -- Open the current file in the default program (on Mac I think this is just `open`)
-keymap.set('n', '<leader>x', ':!open %<cr><cr>')
+keymap.set("n", "<leader>x", ":!open %<cr><cr>")
 
 -- Resize with arrows
-keymap.set('n', '<C-Up>', ':resize +2<CR>')
-keymap.set('n', '<C-Down>', ':resize -2<CR>')
-keymap.set('n', '<C-Left>', ':vertical resize -2<CR>')
-keymap.set('n', '<C-Right>', ':vertical resize +2<CR>')
+keymap.set("n", "<C-Up>", ":resize +2<CR>")
+keymap.set("n", "<C-Down>", ":resize -2<CR>")
+keymap.set("n", "<C-Left>", ":vertical resize -2<CR>")
+keymap.set("n", "<C-Right>", ":vertical resize +2<CR>")
 
 -- Disable arrow keys
-keymap.set('', '<up>', '<nop>')
-keymap.set('', '<down>', '<nop>')
-keymap.set('', '<left>', '<nop>')
-keymap.set('', '<right>', '<nop>')
-keymap.set('i', '<up>', '<nop>')
-keymap.set('i', '<down>', '<nop>')
-keymap.set('i', '<left>', '<nop>')
-keymap.set('i', '<right>', '<nop>')
+keymap.set("", "<up>", "<nop>")
+keymap.set("", "<down>", "<nop>")
+keymap.set("", "<left>", "<nop>")
+keymap.set("", "<right>", "<nop>")
+keymap.set("i", "<up>", "<nop>")
+keymap.set("i", "<down>", "<nop>")
+keymap.set("i", "<left>", "<nop>")
+keymap.set("i", "<right>", "<nop>")
 
-keymap.set('', 'j', 'gj')
-keymap.set('', 'k', 'gk')
+keymap.set("", "j", "gj")
+keymap.set("", "k", "gk")
 
 -- Map Esc to ksh
-keymap.set('i', 'kk', '<Esc>')
+keymap.set("i", "kk", "<Esc>")
 
 -- Map jj to exiting insert mode
-keymap.set('i', 'jj', '<Esc>')
-
+keymap.set("i", "jj", "<Esc>")
 
 -- Clear search highlighting with <leader> and c
-keymap.set('n', '<leader>c', ':nohl<CR>')
+keymap.set("n", "<leader>c", ":nohl<CR>")
 
 -- Change split orientation
-keymap.set('n', '<leader>tk', '<C-w>t<C-w>K') -- change vertical to horizontal
-keymap.set('n', '<leader>th', '<C-w>t<C-w>H') -- change horizontal to vertical
+keymap.set("n", "<leader>tk", "<C-w>t<C-w>K") -- change vertical to horizontal
+keymap.set("n", "<leader>th", "<C-w>t<C-w>H") -- change horizontal to vertical
 
 -- Move around splits using Ctrl + {h,j,k,l}
-keymap.set('n', '<C-h>', '<C-w>h')
-keymap.set('n', '<C-j>', '<C-w>j')
-keymap.set('n', '<C-k>', '<C-w>k')
-keymap.set('n', '<C-l>', '<C-w>l')
+keymap.set("n", "<C-h>", "<C-w>h")
+keymap.set("n", "<C-j>", "<C-w>j")
+keymap.set("n", "<C-k>", "<C-w>k")
+keymap.set("n", "<C-l>", "<C-w>l")
 
 -- Reload configuration without restart nvim
-keymap.set('n', '<leader>r', ':so %<CR>')
+keymap.set("n", "<leader>r", ":so %<CR>")
 
 -- Fast saving with <leader> and s
-keymap.set('n', '<leader>s', ':w<CR>')
-keymap.set('i', '<leader>s', '<C-c>:w<CR>')
+keymap.set("n", "<leader>s", ":w<CR>")
+keymap.set("i", "<leader>s", "<C-c>:w<CR>")
 
 -- Close all windows and exit from Neovim with <leader> and q
-keymap.set('n', '<leader>q', ':qa!<CR>')
+keymap.set("n", "<leader>q", ":qa!<CR>")
 
-keymap.set('i', '<F1>', '<ESC>')
-keymap.set('n', '<F1>', '<ESC>')
-keymap.set('v', '<F1>', '<ESC>')
+keymap.set("i", "<F1>", "<ESC>")
+keymap.set("n", "<F1>", "<ESC>")
+keymap.set("v", "<F1>", "<ESC>")
 
-keymap.set('', '<C-e>', '3<C-e>')
-keymap.set('', '<C-y>', '3<C-y>')
+keymap.set("", "<C-e>", "3<C-e>")
+keymap.set("", "<C-y>", "3<C-y>")
 
 -- Open current directory
-keymap.set('', 'te', ':tabedit<cr>')
+keymap.set("", "te", ":tabedit<cr>")
 
 -- Windows
 
-keymap.set('n', 'ss', ':split<Return><C-w>w')
-keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+keymap.set("n", "ss", ":split<Return><C-w>w")
+keymap.set("n", "sv", ":vsplit<Return><C-w>w")
 
-keymap.set('', '<Space>', '/')
+keymap.set("", "<Space>", "/")
 
 -- Resize window
-keymap.set('', '<C-w><left>', '<C-w><')
-keymap.set('', '<C-w><right>', '<C-w>>')
-keymap.set('', '<C-w><up>', '<C-w>+')
-keymap.set('', '<C-w><down>', '<C-w>-')
+keymap.set("", "<C-w><left>", "<C-w><")
+keymap.set("", "<C-w><right>", "<C-w>>")
+keymap.set("", "<C-w><up>", "<C-w>+")
+keymap.set("", "<C-w><down>", "<C-w>-")
 
 --- Searching and Moving
 -- keymap('n', '/', '/\v')
 -- keymap('v', '/', '/\v')
-keymap.set('', '<leader><space>', ':noh<cr>')
+keymap.set("", "<leader><space>", ":noh<cr>")
 
 --- Close the current buffer
-keymap.set('', '<leader>bd', ':Bclose<cr>:tabclose<cr>gT')
+keymap.set("", "<leader>bd", ":Bclose<cr>:tabclose<cr>gT")
 
 -- Close all buffers
-keymap.set('', '<leader>ba', ':bufdo bd<cr>')
+keymap.set("", "<leader>ba", ":bufdo bd<cr>")
 
 -- Show all buffers
-keymap.set('', '<leader>bb', ':ls<cr>:buffer<Space>')
+keymap.set("", "<leader>bb", ":ls<cr>:buffer<Space>")
 
 -- Move between buffers
-keymap.set('', '“', ':bprev<cr>')
-keymap.set('', '‘', ':bnext<cr>')
+keymap.set("", "“", ":bprev<cr>")
+keymap.set("", "‘", ":bnext<cr>")
 
 -- Move between tabs
-keymap.set('n', '<C-S-Tab>', ':tabprev<cr>')
-keymap.set('n', '<C-Tab>', ':tabnext<cr>')
-keymap.set('n', '<leader>tn', ':tabnew<cr>')
+keymap.set("n", "<C-S-Tab>", ":tabprev<cr>")
+keymap.set("n", "<C-Tab>", ":tabnext<cr>")
+keymap.set("n", "<leader>tn", ":tabnew<cr>")
 
 -- Move between two files
-keymap.set('n', '<leader><Space>', '<C-^>')
+keymap.set("n", "<leader><Space>", "<C-^>")
 
 -- Markdown preview
-keymap.set('n', '<leader>md', ':MarkdownPreview<cr>')
+keymap.set("n", "<leader>md", ":MarkdownPreview<cr>")
 
 -- Move for half page
-keymap.set('n', '<C-u>', '<C-u>zz')
-keymap.set('n', '<C-d>', '<C-d>zz')
-keymap.set('n', 'n', 'nzzzv')
-keymap.set('n', 'N', 'Nzzzv')
+keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
+
+-- vim-maximizer
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window maximization
--- configs/nvim/lua/plugins-setup.lua
+++ configs/nvim/lua/plugins-setup.lua
@@ -1,12 +1,12 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
@@ -20,112 +20,116 @@ vim.cmd([[
   augroup end
 ]])
 
-return require('packer').startup(function(use)
-    -- Let packer manage itself
-    use { 'wbthomason/packer.nvim' }
+return require("packer").startup(function(use)
+	-- Let packer manage itself
+	use({ "wbthomason/packer.nvim" })
 
-    use("nvim-lua/plenary.nvim") -- lua functions that many plugins use
+	use("nvim-lua/plenary.nvim") -- lua functions that many plugins use
 
-    use("christoomey/vim-tmux-navigator") -- tmux & split window navigation
+	use("christoomey/vim-tmux-navigator") -- tmux & split window navigation
 
-    -- essential plugins
-    use("tpope/vim-surround") -- add, delete, change surroundings (it's awesome)
-    use("inkarkat/vim-ReplaceWithRegister") -- replace with register contents using motion (gr + motion)
+	use("szw/vim-maximizer") -- maximizes and restores current window
 
-    -- Statusline
-    use 'nvim-lualine/lualine.nvim'
+	-- essential plugins
+	use("tpope/vim-surround") -- add, delete, change surroundings (it's awesome)
+	use("inkarkat/vim-ReplaceWithRegister") -- replace with register contents using motion (gr + motion)
 
-    -- vscode-like pictograms
-    use 'onsails/lspkind-nvim'
+	-- Statusline
+	use("nvim-lualine/lualine.nvim")
 
-    -- file explorer
-    use("nvim-tree/nvim-tree.lua")
+	-- vscode-like pictograms
+	use("onsails/lspkind-nvim")
 
-    -- vs-code like icons
-    use("nvim-tree/nvim-web-devicons")
+	-- file explorer
+	use("nvim-tree/nvim-tree.lua")
 
-    -- Fuzzy Finder (files, lsp, etc)
-    use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
+	-- vs-code like icons
+	use("nvim-tree/nvim-web-devicons")
 
-    -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
-    use 'nvim-telescope/telescope-file-browser.nvim'
+	-- Fuzzy Finder (files, lsp, etc)
+	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x", requires = { "nvim-lua/plenary.nvim" } })
 
-    -- autocompletion
-    use("hrsh7th/nvim-cmp") -- completion plugin
-    use("hrsh7th/cmp-buffer") -- source for text in buffer
-    use("hrsh7th/cmp-path") -- source for file system paths
+	-- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make", cond = vim.fn.executable("make") == 1 })
+	use("nvim-telescope/telescope-file-browser.nvim")
 
-    -- snippets
-    use("L3MON4D3/LuaSnip") -- snippet engine
-    use("saadparwaiz1/cmp_luasnip") -- for autocompletion
-    use("rafamadriz/friendly-snippets") -- useful snippets
+	-- autocompletion
+	use("hrsh7th/nvim-cmp") -- completion plugin
+	use("hrsh7th/cmp-buffer") -- source for text in buffer
+	use("hrsh7th/cmp-path") -- source for file system paths
 
-    -- managing & installing lsp servers, linters & formatters
-    use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
-    use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
+	-- snippets
+	use("L3MON4D3/LuaSnip") -- snippet engine
+	use("saadparwaiz1/cmp_luasnip") -- for autocompletion
+	use("rafamadriz/friendly-snippets") -- useful snippets
 
-    -- configuring lsp servers
-    use("neovim/nvim-lspconfig") -- easily configure language servers
-    use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
-    use({ "glepnir/lspsaga.nvim", branch = "main" }) -- enhanced lsp uis
-    use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
-    use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
+	-- managing & installing lsp servers, linters & formatters
+	use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
+	use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
 
-    -- formatting & linting
-    use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
-    use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
+	-- configuring lsp servers
+	use("neovim/nvim-lspconfig") -- easily configure language servers
+	use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
+	use({ "glepnir/lspsaga.nvim", branch = "main" }) -- enhanced lsp uis
+	use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
+	use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
 
-    -- treesitter configuration
-    use({
-        "nvim-treesitter/nvim-treesitter",
-        run = function()
-            local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-            ts_update()
-        end,
-    })
+	-- formatting & linting
+	use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
+	use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
 
-    use { -- Additional text objects via treesitter
-        'nvim-treesitter/nvim-treesitter-textobjects',
-        after = 'nvim-treesitter',
-    }
+	-- treesitter configuration
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			ts_update()
+		end,
+	})
 
-    -- auto closing
-    use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
-    use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
+	use({ -- Additional text objects via treesitter
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		after = "nvim-treesitter",
+	})
 
-    -- git integration
-    use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
+	-- auto closing
+	use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
+	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
 
-    use 'folke/zen-mode.nvim'
+	-- git integration
+	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
 
-    use({
-        "iamcco/markdown-preview.nvim",
-        run = function() vim.fn["mkdp#util#install"]() end,
-    })
+	use("folke/zen-mode.nvim")
 
-    use 'akinsho/nvim-bufferline.lua'
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	})
 
-    use 'ray-x/go.nvim'
-    use('theprimeagen/harpoon')
+	use("akinsho/nvim-bufferline.lua")
 
-    use 'ray-x/guihua.lua' -- recommanded if need floating window support
+	use("ray-x/go.nvim")
+	use("theprimeagen/harpoon")
 
-    use { "catppuccin/nvim", as = "catppuccin" }
-    use 'junegunn/goyo.vim'
-    use('mbbill/undotree')
-    use('tpope/vim-fugitive')
-    -- Additional features over rust analyzer
-    use("simrat39/rust-tools.nvim")
+	use("ray-x/guihua.lua") -- recommanded if need floating window support
 
-    -- Colorschemes
-    use { "ellisonleao/gruvbox.nvim" }
-    use 'folke/tokyonight.nvim'
-    use 'navarasu/onedark.nvim'
-    use "EdenEast/nightfox.nvim" -- Packer
-    use 'martinsione/darkplus.nvim'
+	use({ "catppuccin/nvim", as = "catppuccin" })
+	use("junegunn/goyo.vim")
+	use("mbbill/undotree")
+	use("tpope/vim-fugitive")
+	-- Additional features over rust analyzer
+	use("simrat39/rust-tools.nvim")
 
-    if packer_bootstrap then
-        require('packer').sync()
-    end
+	-- Colorschemes
+	use({ "ellisonleao/gruvbox.nvim" })
+	use("folke/tokyonight.nvim")
+	use("navarasu/onedark.nvim")
+	use("EdenEast/nightfox.nvim") -- Packer
+	use("martinsione/darkplus.nvim")
+
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
