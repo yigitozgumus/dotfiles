998d3b43866347097530688017dd4e3c783b699d
Yigit Ozgumus <yigitozgumus1@gmail.com>
2022-12-22 18:33:16 +0300

    delete after plugin

--- configs/nvim/after/plugin/lsp-format.lua
+++
@@ -1,7 +0,0 @@
-local pluginActive = false
-if (not pluginActive) then return end
-
-require("lsp-format").setup {}
-require("lspconfig").gopls.setup { on_attach = require("lsp-format").on_attach }
-require("lspconfig").rust_analyzer.setup { on_attach = require("lsp-format").on_attach }
-
