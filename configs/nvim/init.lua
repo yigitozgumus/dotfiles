de08f3a1091c735f95e595bbcf914232c8816a99
Yigit Ozgumus <yigitozgumus1@gmail.com>
2022-12-27 00:31:48 +0300

    update utils and plugins

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -8,37 +8,4 @@ require("core.autocmds")
 -- Plugin setup
 require("plugins").setup()
 
--- Impatient plugin to speed up the opening
-require("impatient")
-
--- Plugins
-require("config.git")
-require("config.gitsigns")
-require("config.fugitive")
-require("config.nvim-tree")
-require("config.undotree")
-require("config.telescope")
-require("config.zen-mode")
-require("config.markdown-preview")
-require("config.harpoon")
-require("config.go")
-require("config.nvim-cmp")
-require("config.lspkind")
-require("config.comment")
-
--- LSP Related Plugins
-require("config.lsp.mason")
-require("config.lsp.lspsaga")
-require("config.lsp.lspconfig")
-require("config.lsp.null-ls")
-require("config.rust-tools")
-
--- Themes
-require("config.neosolarized")
-require("config.nightfox")
-require("config.gruvbox")
-require("config.catppuccin")
-require("config.tokyonight")
-
--- Colorscheme
 require("core.colorscheme")
