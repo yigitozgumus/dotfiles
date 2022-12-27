2e959d56b005b3afede00aef14146f52867e9691
Yigit Ozgumus <yigitozgumus1@gmail.com>
2022-12-27 23:44:28 +0300

    update plugins

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -6,5 +6,11 @@ vim.g.maplocalleader = ","
 require("config.lazy")
 require("config.base")
 
-require("config.keymaps")
-require("config.autocommands")
+vim.api.nvim_create_autocmd("User", {
+	pattern = "VeryLazy",
+	callback = function()
+		require("util").version()
+		require("config.keymaps")
+		require("config.autocommands")
+	end,
+})
--- configs/nvim/lua/config/plugins/init.lua
+++ configs/nvim/lua/config/plugins/init.lua
@@ -80,4 +80,53 @@ return {
 			require("nvim-navic").setup({ separator = " ", highlight = true, depth_limit = 5 })
 		end,
 	},
+	{
+		"ThePrimeagen/refactoring.nvim",
+		keys = {
+			{
+				"<leader>r",
+				function()
+					require("refactoring").select_refactor()
+				end,
+				mode = "v",
+				noremap = true,
+				silent = true,
+				expr = false,
+			},
+		},
+		config = {},
+	},
+
+	{
+		"simrat39/symbols-outline.nvim",
+		keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+		config = true,
+	},
+
+	{
+		"danymat/neogen",
+		keys = {
+			{
+				"<leader>cc",
+				function()
+					require("neogen").generate({})
+				end,
+				desc = "Neogen Comment",
+			},
+		},
+		config = { snippet_engine = "luasnip" },
+	},
+
+	{
+		"m-demare/hlargs.nvim",
+		event = "VeryLazy",
+		enabled = false,
+		config = {
+			excluded_argnames = {
+				usages = {
+					lua = { "self", "use" },
+				},
+			},
+		},
+	},
 }
