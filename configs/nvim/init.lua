07c353e1818e71962f4c2210ff7c1cd24295a6f6
Yigit Ozgumus <yigitozgumus1@gmail.com>
2022-12-27 23:35:37 +0300

    add some plugins

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -1,12 +1,10 @@
 -- Personal Neovim Configuration
 
-vim.g.mapleader=","
-vim.g.maplocalleader=","
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
 
 require("config.lazy")
 require("config.base")
 
 require("config.keymaps")
 require("config.autocommands")
-
-
