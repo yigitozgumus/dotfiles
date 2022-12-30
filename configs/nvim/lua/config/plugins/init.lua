fde8de85fc425706a8e84094d75cb650969122cd
Yigit Ozgumus <yigitozgumus1@gmail.com>
2022-12-30 21:38:23 +0300

    enable navic

--- configs/nvim/lua/config/plugins/init.lua
+++ configs/nvim/lua/config/plugins/init.lua
@@ -114,7 +114,7 @@ return {
       vim.g.navic_silence = true
       require("nvim-navic").setup({ separator = " ", highlight = true, depth_limit = 5 })
     end,
-    cond = false,
+    cond = true,
     enabled = true,
   },
   {
