f9651ea28b2c4aa8dea243808b46a52d62d9cfbf
Yiğit Özgümüş <yigitozgumus1@gmail.com>
2023-01-15 23:41:31 +0300

    disable package update messaages

--- configs/nvim/lua/config/lazy.lua
+++ configs/nvim/lua/config/lazy.lua
@@ -14,7 +14,7 @@ vim.opt.runtimepath:prepend(lazypath)
 require("lazy").setup("config.plugins", {
   defaults = { lazy = true },
   install = { colorscheme = { "catppuccin", "gruvbox-material", "tokyonight", "habamax" } },
-  checker = { enabled = true },
+  checker = { enabled = true, notify = false },
   diff = {
     cmd = "terminal_git",
   },
