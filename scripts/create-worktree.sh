618b94fa6f8bfa8c6321520f3a585f9f501d6c90
Yigit Ozgumus <yigitozgumus1@gmail.com>
2023-05-08 17:10:31 +0300

    add early exits for create-worktree

--- scripts/create-worktree.sh
+++ scripts/create-worktree.sh
@@ -4,10 +4,25 @@ echo "The Repo is being fetched..."
 git fetch
 # Read the worktree location
 read -p "Worktree Path (Without /, base is your Home Directory): " worktree
+
+if [ -z $worktree ]; then
+	echo "Input is empty, exiting..."
+	exit 1
+fi
 # Get source branch from the Repository
+echo "What will be the source branch?"
 sourceBranch=$(git branch -r | fzf --reverse)
+if [ -z $sourceBranch ]; then
+	echo "Input is empty, exiting..."
+	exit 1
+fi
 echo "Source Branch is $sourceBranch"
 #read -p "Source Branch Name: " branch
 read -p "Target Branch Name: " target
+
+if [ -z $target ]; then
+	echo "Input is empty, exiting..."
+	exit 1
+fi
 # Create the worktree
 $(git worktree add -b $target --checkout $HOME/$worktree $sourceBranch)
