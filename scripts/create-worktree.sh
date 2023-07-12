ed9040691858d100830302340a9d14d1ccc2d242
Yigit Ozgumus <yigitozgumus1@gmail.com>
2023-07-12 12:25:10 +0300

    | Update create work tree and target new branch scripts

--- scripts/create-worktree.sh
+++ scripts/create-worktree.sh
@@ -1,23 +1,49 @@
 #!/bin/bash
+#
+
+if [ -z "$WORKSPACE_DIR" ]; then
+	echo "Workspace Dir value is empty. Set using export WORKSPACE_DIR=<target-dir>"
+	exit 1
+fi
 
 if ! command -v fzf &>/dev/null; then
-	echo "fzf is not found on your system. install it using \033[0;31m brew install fzf"
+	echo -e "fzf is not found on your system. install it using \033[0;31m brew install fzf"
 	exit 1
 fi
 
 # Update the Repository
 echo "The Repo is being fetched..."
-git fetch
+git fetch -q
 # Read the worktree location
-read -p "Worktree Path (Without /, base is your Home Directory): " worktree
+read -p "Worktree Path (Without /, base is $WORKSPACE_DIR): " worktree
 
 if [ -z $worktree ]; then
 	echo "Input is empty, exiting..."
 	exit 1
 fi
+# Decide on branch type
+echo "Branch Type:"
+types="Local\nRemote"
+branchType=$(echo -e $types | fzf)
 # Get source branch from the Repository
-echo "What will be the source branch?"
-sourceBranch=$(git branch -r | fzf --reverse)
+sourceBranch=""
+case "$branchType" in
+"Local")
+	echo "What will be the source branch?"
+	echo "Listing local branches:"
+	sourceBranch=$(git branch | fzf --reverse)
+	;;
+"Remote")
+	echo "What will be the source branch?"
+	echo "Listing remote branches:"
+	sourceBranch=$(git branch -r | fzf --reverse)
+	;;
+*)
+	echo "Invalid Choice. exiting..."
+	exit 1
+	;;
+
+esac
 if [ -z $sourceBranch ]; then
 	echo "Input is empty, exiting..."
 	exit 1
@@ -31,4 +57,4 @@ if [ -z $target ]; then
 	exit 1
 fi
 # Create the worktree
-$(git worktree add -b $target --checkout $HOME/$worktree $sourceBranch)
+git worktree add -b $target $WORKSPACE_DIR/$worktree $sourceBranch
--- scripts/target-new-branch.sh
+++ scripts/target-new-branch.sh
@@ -1,40 +1,56 @@
 #!/bin/bash
 
 # Update the Repository
-git fetch
+echo "Fetching the latest version..."
+git fetch --quiet
 
-echo "What will be the source branch?"
-sourceBranch=$(git branch -r | fzf --reverse)
+echo "What will be the new upstream?"
+newUpstream=$(git branch -r | fzf --reverse)
 
-if [ -z $sourceBranch ]; then
+if [ -z $newUpstream ]; then
 	echo "Input is empty, exiting..."
 	exit 1
 fi
 
-# Read targetBranch
-read -p "What is the target branch's name?: " targetBranch
+echo "New Upstream is $newUpstream"
 
-if [ -z $targetBranch ]; then
+# Get current branch
+branchName=$(git branch --show-current)
+
+echo "What is the current upstream?"
+currentUpstream=$(git branch -r | fzf --reverse)
+
+if [ -z $currentUpstream ]; then
 	echo "Input is empty, exiting..."
 	exit 1
 fi
+
+echo "Current Upstream is $currentUpstream"
+
+targetBranch="${branchName}-RC"
+echo "New target branch is $targetBranch"
 
 # Get Commit list
 
 userName=$(git config user.name)
-branchName=$(git branch --show-current)
 branchPrefix=$(echo $branchName | grep -Eo '^[A-Z0-9\-]+')
-commitList=$(git log --author=$userName --grep=$branchPrefix --oneline --no-merges)
+commitList=$(git log --author="$userName" --grep="$branchPrefix" --oneline --no-merges)
+
+git checkout $currentUpstream
+git checkout -b $targetBranch
+
+echo "I created $targetBranch from $currentUpstream"
 
 # Get commit list inside the branch
-commitIDList=$(echo $commitList | awk '{print $1}' | awk '{ lines[NR] = $0 } END { for (i = NR; i >= 1; i--) print lines[i] }  ')
+commitIDList=$(echo "$commitList" | awk '{print $1}' | awk '{ lines[NR] = $0 } END { for (i = NR; i >= 1; i--) print lines[i] } ')
 
-currentBranch=$(git branch --show-current)
-
-git checkout $sourceBranch
-git checkout -b $targetBranch
 for commit in $commitIDList; do
 	if [ -n "$commit" ]; then
 		git cherry-pick $commit
 	fi
 done
+
+echo "Commits are applied."
+
+echo "Rebase operation is started."
+git rebase --onto $newUpstream $currentUpstream $targetBranch
