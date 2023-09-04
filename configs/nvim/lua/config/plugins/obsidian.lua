d1476e597cb30da9864d8968a4cea4ce523f0d07
Yiğit Özgümüş <yigitozgumus1@gmail.com>
2023-09-05 00:32:19 +0300

    Add obsidian plugin

---
+++ configs/nvim/lua/config/plugins/obsidian.lua
@@ -0,0 +1,18 @@
+return {
+  "epwalsh/obsidian.nvim",
+  lazy = true,
+  event = {
+    -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+    "BufReadPre  /Users/yigitozgumus/Documents/Notes/Personal/**.md",
+    "BufNewFile /Users/yigitozgumus/Documents/Notes/Personal/**.md",
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  mappings = {},
+  opts = {
+    dir = "~/Documents/Notes/Personal", -- no need to call 'vim.fn.expand' here
+  },
+}
