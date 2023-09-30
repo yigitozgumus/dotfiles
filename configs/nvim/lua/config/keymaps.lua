93215d21c8e89c46fa61e479fe2a3511d73ac9bd
Yiğit Özgümüş <yigitozgumus1@gmail.com>
2023-09-30 13:22:30 +0300

    update nvim

--- configs/nvim/lua/config/keymaps.lua
+++ configs/nvim/lua/config/keymaps.lua
@@ -88,3 +88,11 @@ keymap("n", "N", "Nzzzv", default_opts)
 
 -- restart lsp server
 keymap("n", "<leader>rs", ":LspRestart<CR>", default_opts) -- mapping to restart lsp if necessary
+
+vim.keymap.set("n", "<leader>/", function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+    winblend = 10,
+    previewer = false,
+  }))
+end, { desc = "[/] Fuzzily search in current buffer" })
