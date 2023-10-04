d0a65413c86712efdf3034acdb6d5513b85e022b
Yigit Ozgumus <yigitozgumus1@gmail.com>
2023-10-04 09:05:07 +0300

    Update neovide font size

--- configs/nvim/lua/config/base.lua
+++ configs/nvim/lua/config/base.lua
@@ -4,144 +4,144 @@
 
 local options = {
 
-	-- Output Encoding that is shown on the terminal
-	encoding = "utf-8",
+  -- Output Encoding that is shown on the terminal
+  encoding = "utf-8",
 
-	-- Output Encoding of that file that is written
-	fileencoding = "utf-8",
+  -- Output Encoding of that file that is written
+  fileencoding = "utf-8",
 
-	-- allow mouse to be used in neovim
-	mouse = "a",
+  -- allow mouse to be used in neovim
+  mouse = "a",
 
-	-- creates a swapfile
-	swapfile = false,
+  -- creates a swapfile
+  swapfile = false,
 
-	-- time towait for a mapped sequence to complete
-	timeoutlen = 1000,
+  -- time towait for a mapped sequence to complete
+  timeoutlen = 1000,
 
-	-- enable persistent undo
-	undofile = true,
-	title = true,
+  -- enable persistent undo
+  undofile = true,
+  title = true,
 
-	-- highlight all matches on previous search pattern
-	hlsearch = true,
+  -- highlight all matches on previous search pattern
+  hlsearch = true,
 
-	-- Creates a backup file
-	backup = false,
+  -- Creates a backup file
+  backup = false,
 
-	showcmd = true,
+  showcmd = true,
 
-	-- more space in the neovim command line
-	cmdheight = 1,
+  -- more space in the neovim command line
+  cmdheight = 1,
 
-	-- Set global statusline
-	laststatus = 2,
+  -- Set global statusline
+  laststatus = 2,
 
-	number = true,
+  number = true,
 
-	showmatch = true,
+  showmatch = true,
 
-	relativenumber = true,
+  relativenumber = true,
 
-	-- force all vertical splits to go to the right
-	splitright = true,
+  -- force all vertical splits to go to the right
+  splitright = true,
 
-	-- force all horizontal splits to go below current
-	splitbelow = true,
+  -- force all horizontal splits to go below current
+  splitbelow = true,
 
-	-- ignore case in search patterns
-	ignorecase = true,
+  -- ignore case in search patterns
+  ignorecase = true,
 
-	-- Smart case
-	smartcase = true,
+  -- Smart case
+  smartcase = true,
 
-	linebreak = true,
+  linebreak = true,
 
-	nrformats = "",
+  nrformats = "",
 
-	-- No need to see mode
-	showmode = false,
+  -- No need to see mode
+  showmode = false,
 
-	-- always show tabs
-	showtabline = 2,
+  -- always show tabs
+  showtabline = 2,
 
-	-- Used for cmp
-	completeopt = { "menuone", "noselect" },
+  -- Used for cmp
+  completeopt = { "menuone", "noselect" },
 
-	-- Backticks are visible in markdown
-	conceallevel = 0,
+  -- Backticks are visible in markdown
+  conceallevel = 0,
 
-	inccommand = "split",
+  inccommand = "split",
 
-	backupskip = { "/tmp/*", "/private/tmp/*" },
+  backupskip = { "/tmp/*", "/private/tmp/*" },
 
-	backspace = { "start", "eol", "indent" },
+  backspace = { "start", "eol", "indent" },
 
-	-- if a file is being edited by another program
-	writebackup = false,
+  -- if a file is being edited by another program
+  writebackup = false,
 
-	-- convert tabs to spaces
-	expandtab = true,
+  -- convert tabs to spaces
+  expandtab = true,
 
-	-- the number of spaces inserted for each indentation
-	shiftwidth = 4,
+  -- the number of spaces inserted for each indentation
+  shiftwidth = 4,
 
-	-- insert 2 spaces for a tab
-	tabstop = 4,
+  -- insert 2 spaces for a tab
+  tabstop = 4,
 
-	-- make indenting smarter
-	smartindent = true,
+  -- make indenting smarter
+  smartindent = true,
 
-	autoindent = true,
+  autoindent = true,
 
-	wrap = true,
+  wrap = true,
 
-	-- Enable background buffers
-	hidden = true,
+  -- Enable background buffers
+  hidden = true,
 
-	-- Remember N lines in history
-	history = 10000,
+  -- Remember N lines in history
+  history = 10000,
 
-	-- Max column for syntax highlight
-	synmaxcol = 240,
+  -- Max column for syntax highlight
+  synmaxcol = 240,
 
-	-- ms to wait for trigger an event
-	updatetime = 400,
+  -- ms to wait for trigger an event
+  updatetime = 400,
 
-	-- highlight the current line
-	cursorline = true,
+  -- highlight the current line
+  cursorline = true,
 
-	termguicolors = true,
+  termguicolors = true,
 
-	winblend = 0,
+  winblend = 0,
 
-	wildoptions = "pum",
+  wildoptions = "pum",
 
-	pumblend = 5,
+  pumblend = 5,
 
-	-- pop up menu height
-	pumheight = 10,
+  -- pop up menu height
+  pumheight = 10,
 
-	background = "dark",
+  background = "dark",
 
-	-- always show the sign column, otherwise it would shift the text each time
-	signcolumn = "yes",
+  -- always show the sign column, otherwise it would shift the text each time
+  signcolumn = "yes",
 
-	wildmode = "longest:full,full",
+  wildmode = "longest:full,full",
 
-	scrolloff = 10,
+  scrolloff = 10,
 
-	sidescrolloff = 10,
+  sidescrolloff = 10,
 
-	colorcolumn = "80",
+  colorcolumn = "80",
 
-	confirm = true,
+  confirm = true,
 
-	fillchars = "eob: ",
+  fillchars = "eob: ",
 }
 
 for option, value in pairs(options) do
-	vim.opt[option] = value
+  vim.opt[option] = value
 end
 
 -- allows neovim to access to system clipboard
@@ -154,6 +154,7 @@ vim.opt.wildignore:append({ "*/node_modules/*" })
 
 -- Turn off paste mode when leaving insert
 vim.api.nvim_create_autocmd("InsertLeave", {
-	pattern = "*",
-	command = "set nopaste",
+  pattern = "*",
+  command = "set nopaste",
 })
+vim.opt.guifont = { "FiraCode Nerd Font", ":h20" }
