e4d5638b8f7555349baaca7da87190e2c0bd06ef
Yiğit Özgümüş <yigitozgumus1@gmail.com>
2023-11-11 14:57:06 +0300

    | Add wezterm configuration

---
+++ configs/wezterm/wezterm.lua
@@ -0,0 +1,169 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+local wezterm_action = wezterm.action
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+local function _run_command(cmd)
+	local f = assert(io.popen(cmd, "r"))
+	local s = assert(f:read("*a"))
+	f:close()
+	s = string.gsub(s, "^%s+", "")
+	s = string.gsub(s, "%s+$", "")
+	s = string.gsub(s, "[\n\r]+", "")
+	return s
+end
+
+local function _home_dir()
+	return string.format("file://%s", os.getenv("HOME"))
+end
+
+local function _current_dir(current_uri)
+	local homedir = _home_dir()
+	if current_uri:find("^" .. homedir) ~= nil then
+		return "~" .. current_uri:sub(#homedir + 1, #current_uri)
+	else
+		return current_uri:sub(#"file:///", #current_uri)
+	end
+end
+
+--- ========== Keys ==========
+config.keys = {}
+table.insert(config.keys, { key = "LeftArrow", mods = "SHIFT|SUPER", action = wezterm_action.ActivateTabRelative(-1) })
+table.insert(config.keys, { key = "RightArrow", mods = "SHIFT|SUPER", action = wezterm_action.ActivateTabRelative(1) })
+
+--- ========== Fonts & Themes ==========
+local FiraCodeFont = "FiraCode Nerd Font Mono"
+local CodeNewRomanFont = "CodeNewRoman Nerd Font"
+local MonacoFont = "Monaco Nerd Font Mono"
+local NotoSansMonoFont = "NotoSansMono NFM"
+local IosevakaFont = "Iosevka Nerd Font Mono"
+local HackFont = "Hack Nerd Font Mono"
+local FantasqueSansMonoFont = "FantasqueSansMono NFM"
+local SauceCodeProFont = "SauceCodePro Nerd Font Mono"
+
+local CatppuccinMochaTheme = "Catppuccin Mocha"
+local GruvboxDarkTheme = "Gruvbox Dark (Gogh)"
+
+config.font = wezterm.font(FiraCodeFont)
+
+config.font_size = 15.0
+if IS_WINDOWS then
+	config.font_size = 14.0
+end
+if IS_LINUX then
+	config.font_size = 11.0
+end
+config.colors = {
+	scrollbar_thumb = "black",
+}
+
+-- This is where you actually apply your config choices
+config.color_scheme = CatppuccinMochaTheme
+
+--- ========== Tabs ==========
+config.use_fancy_tab_bar = true
+config.tab_max_width = 256
+config.window_frame = {
+	font = wezterm.font(FiraCodeFont),
+	font_size = 13,
+}
+config.scrollback_lines = 200000
+config.enable_scroll_bar = true
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+config.window_decorations = "RESIZE"
+
+local function get_process(tab)
+	local process_name = string.gsub(tab.active_pane.foreground_process_name, "(.*[/\\])(.*)", "%2")
+	if string.find(process_name, "kubectl") then
+		process_name = "kubectl"
+	end
+	return wezterm.format({ { Text = string.format("[%s]", process_name) } })
+end
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local function _get_cwd(tab)
+		return _current_dir(tab.active_pane.current_working_dir)
+	end
+
+	local title = string.format(" %s %s ", get_process(tab), _get_cwd(tab))
+	return {
+		{ Text = title },
+	}
+end)
+
+--- ========== Right Status ==========
+
+wezterm.on("update-right-status", function(window, pane)
+	local SOLID_LEFT_ARROW = ""
+	local SOLID_RIGHT_ARROW = ""
+	local COLORS = {
+		"#3c1361",
+		"#52307c",
+		"#663a82",
+		"#7c5295",
+		"#b491c8",
+	}
+	local TEXT_FG = "#c0c0c0"
+
+	local cells = {}
+	local cwd_uri = pane:get_current_working_dir()
+	local cwd = nil
+	if type(cwd_uri) == "userdata" then
+		cwd = cwd_uri.file_path
+	else
+		cwd = _current_dir(cwd_uri)
+	end
+	table.insert(cells, cwd)
+
+	local date = wezterm.strftime("%a %b %-d %H:%M")
+	table.insert(cells, date)
+
+	local elements = {}
+	local num_cells = 0
+
+	table.insert(elements, { Foreground = { Color = "#3c1361" } })
+
+	table.insert(elements, { Text = SOLID_RIGHT_ARROW })
+
+	local function _push(text, is_last)
+		local cell_no = num_cells + 1
+		table.insert(elements, { Foreground = { Color = TEXT_FG } })
+		table.insert(elements, { Background = { Color = COLORS[cell_no] } })
+		table.insert(elements, { Text = " " .. text .. " " })
+		if not is_last then
+			table.insert(elements, { Foreground = { Color = COLORS[cell_no + 1] } })
+			table.insert(elements, { Text = SOLID_RIGHT_ARROW })
+		end
+		num_cells = num_cells + 1
+	end
+
+	while #cells > 0 do
+		local cell = table.remove(cells, 1)
+		_push(cell, #cells == 0)
+	end
+
+	window:set_right_status(wezterm.format(elements))
+end)
+
+--- ========== CWD ==========
+config.default_cwd = wezterm.home_dir
+
+-- For example, changing the color scheme:
+config.window_background_opacity = 0.95
+
+-- and finally, return the configuration to wezterm
+return config
