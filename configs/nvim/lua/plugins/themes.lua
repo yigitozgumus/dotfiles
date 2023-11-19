f2adc51d1e36b93d7b37ab221ba3e97c3f49383c
Yiğit Özgümüş <yigitozgumus1@gmail.com>
2023-11-19 17:25:49 +0300

    | Update lazyvim plugins and zsh updates

---
+++ configs/nvim/lua/plugins/themes.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    config = function()
+      -- vim.cmd.colorscheme("tokyonight")
+    end,
+  },
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "martinsione/darkplus.nvim",
+  },
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+
+        -- transparent_background = true,
+        -- term_colors = false,
+        dim_inactive = {
+          enabled = false,
+          shade = "dark",
+          percentage = 0.15,
+        },
+      })
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    priority = 1000,
+    config = function()
+      -- vim.cmd.colorscheme("kanagawa")
+    end,
+  },
+  {
+    "sainnhe/gruvbox-material",
+    priority = 1000,
+    config = function()
+      -- vim.cmd.colorscheme("gruvbox-material")
+    end,
+  },
+}
