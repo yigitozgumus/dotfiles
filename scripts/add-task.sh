4bd40eeada455ef275fae1fd3170cf7810e47fd0
Yigit Ozgumus <yigitozgumus1@gmail.com>
2024-08-26 10:01:57 +0300

    Activate add task script

--- scripts/add-task.sh
+++ scripts/add-task.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-test =`task _projects | fzf`
-echo $test
-# read -p "Task: " taskContent
-# current=$taskContent
-# read -p "Due: " dueTime
-# if [ "$dueTime" != "" ]; then
-# 	current="${current} due:${dueTime}"
-# fi
-# read -p "Tags: " tags
-# if [ "$tags" != "" ]; then
-# 	current="${current} tag:${tags}"
-# fi
-#
-# read -p "Project: " project
-# if [ "$project" != "" ]; then
-# 	current="${current} project:${project}"
-# fi
-#
-# task add $current
+read -p "Task: " taskContent
+current=$taskContent
+read -p "Due: " dueTime
+if [ "$dueTime" != "" ]; then
+	current="${current} due:${dueTime}"
+fi
+read -p "Tags: " tags
+if [ "$tags" != "" ]; then
+	current="${current} tag:${tags}"
+fi
+
+read -p "Project: " project
+if [ "$project" != "" ]; then
+	current="${current} project:${project}"
+fi
+
+task add $current
